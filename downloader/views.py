f74278e63f156ef5f70d579dab57b0010d398c06
SOMANATH-2039 <somanathsingh0803@gmail.com>
2024-02-13 01:06:45 +0530

    All done

---
+++ downloader/views.py
@@ -0,0 +1,44 @@
+# importing all the required modules 
+from django.shortcuts import render, redirect 
+from pytube import *
+import os
+
+
+DOWNLOAD_DIR='D'
+# defining function 
+def playlistYoutube(request): 
+
+	# checking whether request.method is post or not 
+	if request.method == 'POST': 
+		
+		# getting link from frontend 
+		links = request.POST['link']
+		playlist=Playlist(links)
+		for link in playlist.video_urls:
+			video = YouTube(link) 
+
+			# setting video resolution 
+			stream = video.streams.get_highest_resolution() 
+			
+			# downloads video 
+			stream.download() 
+			downloaded+=1
+		
+				# returning HTML page 
+		# 	return render(request, 'youtube.html') 
+	return render(request, 'youtube.html')
+
+def oneYoutube(request):
+	if request.method =='POST':
+		link = request.POST['link']
+		video = YouTube(link) 
+		# setting video resolution 
+		stream = video.streams.get_highest_resolution() 
+		# downloads video 
+		stream.download()
+
+	return render(request, 'youtube.html')
+
+def home(request):
+
+	return render(request, 'home.html')
